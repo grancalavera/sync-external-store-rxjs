f4122202230a3bbb634bb5780d5338b60c332d1f
Leon Coto <leoncoto@gmail.com>
2024-08-18 15:01:33 +0100

    some progress on a different direction but stuck again

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "modern-normalize/modern-normalize.css";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import Example from "./Example14";
+import Example from "./Example15";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
